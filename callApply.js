3d394f3c1f8aa848ee67dbc0b31eac6a156ada7b
Tamim Mahmud <internationalinf24@gmail.com>
2022-07-15 23:07:53 +0600

    exit multiple result by use one function in js

--- callApply.js
+++ callApply.js
@@ -1,12 +1,12 @@
-const simplePerson = {
-    firstName : 'Golam',
-    lastName : 'Masum',
-    totalName : function(){
-        console.log(this.firstName, this.lastName);
-    },
-};
-console.log(simplePerson);
-console.log(simplePerson.firstName);
+// const simplePerson = {
+//     firstName : 'Golam',
+//     lastName : 'Masum',
+//     totalName : function(){
+//         console.log(this.firstName, this.lastName);
+//     },
+// };
+// console.log(simplePerson);
+// console.log(simplePerson.firstName);
 
 const smartPerson = {
     firstNameis : 'Sanam',
@@ -16,14 +16,28 @@ const smartPerson = {
         console.log(this.firstNameis, this.firstNameis.lastNameis);
     },
     billAmount : function(amount){
+        // console.log(this);
         this.salary -= amount;
         return this.salary;
     }
 
 };
 
-let result = smartPerson.billAmount(140);
-smartPerson.billAmount(3000);
-console.log(result);
+const heroPerson = {
+    firstNameis : 'Hero',
+    lastNameis : 'Alom',
+    salary : 15400,
+};
+// console.log(heroPerson);
+// let result = smartPerson.billAmount(140);
+// smartPerson.billAmount(3000);
+// console.log(result);
 
-console.log(smartPerson.salary);
+// console.log(smartPerson.salary);
+
+
+// smartPerson.billAmount();
+const heroSalaryBill = smartPerson.billAmount.bind(heroPerson);
+heroSalaryBill(2000); 
+heroSalaryBill(2000); 
+console.log(heroPerson.salary);
